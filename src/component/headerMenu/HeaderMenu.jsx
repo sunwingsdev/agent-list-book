b218a89be0e6207a3e199e8111db6041df968873
vismodeb <vismodeb2000@gmail.com>
2024-05-30 12:30:33 +0600

    Agent List Book Project created

---
+++ src/component/headerMenu/HeaderMenu.jsx
@@ -0,0 +1,40 @@
+import "./headermenu.css";
+import logo from "../../assets/Layer-1.png";
+import { Typewriter } from "react-simple-typewriter";
+import { AiTwotoneAudio } from "react-icons/ai";
+import Marquee from "react-fast-marquee";
+import NavbarMenu from "../navbarMenu/NavbarMenu";
+
+const HeaderMenu = () => {
+  return (
+    <div className="container">
+      <div className="headerTopMenu">
+        <div className="headerLeftImg">
+          <img src={logo} alt="" />
+        </div>
+        <div className="headerRightText">
+          <Typewriter
+            words={["OFFICIAL AGENT LIST"]}
+            loop={0}
+            cursor={true}
+            cursorColor="#ffdf6e"
+          />
+        </div>
+      </div>
+      <div className="headerMarquee">
+        <div className="marqueeTitle">
+          <AiTwotoneAudio className="marqueeSize" />
+          <h2>NEWS</h2>
+          <span></span>
+        </div>
+        <Marquee className="marqueeText">
+          Welcome to our 11wickets official agentlist..Thanks For Stay With
+          Tamim Khan Family...
+        </Marquee>
+      </div>
+      <NavbarMenu />
+    </div>
+  );
+};
+
+export default HeaderMenu;
---
+++ src/component/home/TabData.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable react/prop-types */
+import { Table } from "react-bootstrap";
+import { FaWhatsappSquare } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import "../navbarMenu/navbarmenu.css";
+
+const TabData = ({ tableHeading }) => {
+  return (
+    <div className="tabContain_2">
+      <div className="tabContainItem_2">
+        <h2>{tableHeading}</h2>
+        <form action="">
+          <lable>Search:</lable>
+          <input type="text" placeholder="Search.." />
+        </form>
+        <div className="table-responsive">
+          <Table striped bordered hover>
+            <thead>
+              <tr className="text-center tableThBox">
+                <th>TYPE</th>
+                <th>ID</th>
+                <th>PHONE APP LINK</th>
+                <th>NUMBER</th>
+                <th>COMPLAIN</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="text-center">
+                <td>Tamim Khan</td>
+                <td>001</td>
+                <td>
+                  <Link>
+                    <FaWhatsappSquare className="whatsAppIcon whatsAppIcon_2" />
+                  </Link>
+                </td>
+                <td>+971556850160</td>
+                <td>COMPLAIN</td>
+              </tr>
+            </tbody>
+          </Table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TabData;
