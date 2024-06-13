a950109ef471fbb6f54c82033bd10dd2566ba389
simanto97 <saimumsimanto97@gmail.com>
2024-06-13 11:31:22 +0600

    add remove functionality

--- src/component/dashboard/aside/Aside.jsx
+++ src/component/dashboard/aside/Aside.jsx
@@ -1,67 +1,91 @@
+import { useState } from "react";
 import "./Aside.css";
 import { Link } from "react-router-dom";
 import dashboard_logo from "../../../assets/Layer-1.png";
-import { FaHome, FaUser, FaAffiliatetheme, FaMinus } from "react-icons/fa";
+import {
+  FaHome,
+  FaUser,
+  FaAffiliatetheme,
+  FaMinus,
+  FaPlus,
+} from "react-icons/fa";
 
 const Aside = () => {
+  // State for collapsing menus
+  const [menuCollapsed, setMenuCollapsed] = useState({
+    theme: true,
+    homeContents: true,
+  });
+
+  const toggleMenu = (menu) => {
+    setMenuCollapsed((prevState) => ({
+      ...prevState,
+      [menu]: !prevState[menu],
+    }));
+  };
+
   return (
     <div className="aside">
       <div className="dashboardLogoImg">
-        <Link to={"/"}>
-          <img src={dashboard_logo} alt="" />
+        <Link to="/">
+          <img src={dashboard_logo} alt="Dashboard Logo" />
         </Link>
       </div>
-      <h2 className="DBText">DashBoard </h2>
-      {/* <div className="dashboardMenu"> */}
+      <h2 className="DBText">DashBoard</h2>
       <ul className="dashboardMenu">
-        <Link>
+        <Link to="/">
           <li className="active">
             <FaHome />
             Home
           </li>
         </Link>
-        <Link>
+        <Link to="/user">
           <li>
             <FaUser />
             User
           </li>
         </Link>
-        <Link>
-          <li className="bSubMenu">
-            <div className="d-flex align-items-center gap-2">
-              <FaAffiliatetheme />
-              Theme
-            </div>
-            <FaMinus />
-          </li>
-          <ul className="dashboardSubMenu">
-            <Link to={"/dashboard/logo"}>
+        <li className="bSubMenu" onClick={() => toggleMenu("theme")}>
+          <div className="d-flex align-items-center gap-2">
+            <FaAffiliatetheme />
+            Theme
+          </div>
+          <div>{menuCollapsed.theme ? <FaPlus /> : <FaMinus />}</div>
+        </li>
+
+        {!menuCollapsed.theme && (
+          <ul
+            className={`dashboardSubMenu ${
+              menuCollapsed.theme ? "" : "expanded"
+            }`}
+          >
+            <Link to="/dashboard/logo">
               <li>▪ Logo</li>
             </Link>
-            <Link>
+            <Link to="/dashboard/user">
               <li>▪ User</li>
             </Link>
           </ul>
-        </Link>
-        <Link>
-          <li className="bSubMenu">
-            <div className="d-flex align-items-center gap-2">
-              {/* <FaAffiliatetheme /> */}
-              Home Contents
-            </div>
-            <FaMinus />
-          </li>
-          <ul className="dashboardSubMenu">
-            <Link to={"edit-home"}>
+        )}
+        <li className="bSubMenu" onClick={() => toggleMenu("homeContents")}>
+          <div className="d-flex align-items-center gap-2">Home Contents</div>
+          {menuCollapsed.homeContents ? <FaPlus /> : <FaMinus />}
+        </li>
+        {!menuCollapsed.homeContents && (
+          <ul
+            className={`dashboardSubMenu ${
+              menuCollapsed.homeContents ? "" : "expanded"
+            }`}
+          >
+            <Link to="/dashboard/edit-home">
               <li>▪ Home</li>
             </Link>
-            <Link>
+            <Link to="/dashboard/user">
               <li>▪ User</li>
             </Link>
           </ul>
-        </Link>
+        )}
       </ul>
-      {/* </div> */}
     </div>
   );
 };
