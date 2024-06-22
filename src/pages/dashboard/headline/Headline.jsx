71beb733503be97f69caf1345e90755d11d0758c
vismodeb <vismodeb2000@gmail.com>
2024-06-22 18:39:07 +0600

    Headlist some code add

--- src/pages/dashboard/headline/Headline.jsx
+++ src/pages/dashboard/headline/Headline.jsx
@@ -1,24 +1,48 @@
-import "../../../component/dashboard/dataInput/DataInput.css";
+import { AiTwotoneAudio } from "react-icons/ai";
+import "./Headline.css";
+import Marquee from "react-fast-marquee";
 
 const Headline = () => {
   return (
     <div className="">
-      <div className="dataInputText">
+      <div className="headlineAreaInput mb-4">
         <form className="form" action="">
-          <input
-            type="text"
-            placeholder="Headline"
-            className="form__input"
-            id="text"
-          />
-          <label htmlFor="headline" className="form__label">
-            Headline
-          </label>
+          <div className="form-group mb-2">
+            <label htmlFor="Title">Title</label>
+            <input
+              type="text"
+              className="form-control"
+              id="Title"
+              aria-describedby="emailHelp"
+              placeholder="Title"
+            />
+          </div>
+          <div className="form-group mb-3">
+            <label htmlFor="headline">Headline</label>
+            <input
+              type="text"
+              className="form-control"
+              id="headline"
+              aria-describedby="emailHelp"
+              placeholder="Headline"
+            />
+          </div>
 
           <button type="submit" className="btn btn-primary">
             Submit
           </button>
         </form>
+      </div>
+      <div className="headerMarquee">
+        <div className="marqueeTitle">
+          <AiTwotoneAudio className="marqueeSize" />
+          <h2>NEWS</h2>
+          <span></span>
+        </div>
+        <Marquee className="marqueeText">
+          Welcome to our 11wickets official agentlist..Thanks For Stay With
+          Tamim Khan Family...
+        </Marquee>
       </div>
     </div>
   );
